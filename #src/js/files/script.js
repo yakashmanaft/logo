a69a48e19f4257c17410fcc2dd2193bf4c98c542
Sergey <anfalov@camini-pr.ru>
2020-10-25 18:37:52 +0500

    Проходим четвертую часть

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -62,3 +62,35 @@ for (let index = 0; index < checkboxCategories.length; index++) {
         }
     });
 }
+
+const priceSlider = document.querySelector('.price-filter__slider');
+
+noUiSlider.create(priceSlider, {
+    start: [0, 200000],
+    connect: true,
+    tooltips: [wNumb({decimals: 0}), wNumb({decimals: 0})],
+    range: {
+        'min': 0,
+        'max': 200000
+    }
+});
+
+const priceStart = document.getElementById('price-start');
+const priceEnd = document.getElementById('price-end');
+
+priceStart.addEventListener('change', setPriceValues);
+
+priceEnd.addEventListener('change', setPriceValues);
+
+function setPriceValues() {
+    let priceStartValue;
+    let priceEndValue;
+    if (priceStart.value != '') {
+        priceStartValue = priceStart.value;
+    }
+    if (priceEnd.value != '') {
+        priceEndValue = priceEnd.value;
+    }
+
+    priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
+}
--- #src/js/vendors.js
+++ #src/js/vendors.js
@@ -1,2 +1,4 @@
+@@include('libs/wNumb.min.js', {})
 @@include('libs/smoothScroll.js', {})
 @@include('libs/swiper.min.js', {})
+@@include('libs/nouislider.js', {})
